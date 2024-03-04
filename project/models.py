d0ee2831b55e882957f1999e196a4191b236b6bf
Arsenij <kuprin.ars@gmail.com>
2024-03-05 00:57:40 +0100

    edits

--- project/models.py
+++ project/models.py
@@ -11,3 +11,20 @@ class User(UserMixin, db.Model):
 #     id = db.Column(db.Integer, primary_key = True)
 #     email = db.Column(db.String(100))
 #     name = db.Column(db.String(100))
+#     comp_id = db.Colimn(db.Interger)
+
+# class Competition(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     name = db.Column(db.String)
+#     event_1 = db.Column(db.String(20))
+#     event_2 = db.Column(db.String(20))
+#     event_3 = db.Column(db.String(20))
+#     event_4 = db.Column(db.String(20))
+
+
+
+# class Result(db.Model):
+#     id = db.Column(db.Integer, primary_key = True)
+#     competor_id = db.Comlumn()
+
+
